80f42badcedcaaf0943f5d3019b9f0a025a94c08
Brenda <brendaprofiro@gmail.com>
2021-02-13 12:21:18 -0300

    Create Mirror component

---
+++ web/src/components/Mirror/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Mirror: React.FunctionComponent = () => {
+  return (
+    <div>
+      <span>
+        <b>Mirror: </b>
+      </span>
+    </div>
+  );
+}
+
+export default Mirror;
