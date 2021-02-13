375db3260cf53a4f8c12a0d67cd3cb74b9f5c32c
Brenda <brendaprofiro@gmail.com>
2021-02-13 12:44:54 -0300

    Enable CountContext into components

--- web/src/components/Counter/index.tsx
+++ web/src/components/Counter/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
+import { useCount } from "../../context/CountContext";
+
 const Counter: React.FunctionComponent = () => {
+  const { count, setCount } = useCount();
+
   return (
     <div>
       <span>
         <b>Count: </b>
+        {count}
       </span>
 
       <br />
 
-      <button>
-        Change theme
+      <button onClick={() => setCount(count + 1)}>
+        Increase
       </button>
     </div>
   );
--- web/src/components/Mirror/index.tsx
+++ web/src/components/Mirror/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useCount } from '../../context/CountContext';
 
 const Mirror: React.FunctionComponent = () => {
+  const { count } = useCount()
+
   return (
     <div>
       <span>
         <b>Mirror: </b>
+        {count}
       </span>
     </div>
   );
