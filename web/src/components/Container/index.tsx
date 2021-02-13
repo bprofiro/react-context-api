2ee031cf5f3e7b6dc9b3607e670eee1a0701aea1
Brenda <brendaprofiro@gmail.com>
2021-02-13 12:54:48 -0300

    Enable ThemeContext into component

--- web/src/components/Container/index.tsx
+++ web/src/components/Container/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { themes, useTheme } from '../../context/ThemeContext'
 
 const Container: React.FunctionComponent = ({ children }) => {
+  const { theme, setTheme } = useTheme()
+
   return (
-    <div>
+    <div style={{
+      width: '100vw',
+      height: '100vh',
+      background: theme.color.background,
+      color: theme.color.text,
+    }}>
       {children}
 
       <hr />
 
-      <button>
+      <button onClick={() => setTheme(theme.name === 'dark' ? themes[1] : themes[0])}>
         Change theme
       </button>
     </div>
