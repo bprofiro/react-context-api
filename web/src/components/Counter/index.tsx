aa076a78be7892b45546794f3a3be8d76d139cae
Brenda <brendaprofiro@gmail.com>
2021-02-13 12:21:07 -0300

    Create Counter component

---
+++ web/src/components/Counter/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Counter: React.FunctionComponent = () => {
+  return (
+    <div>
+      <span>
+        <b>Count: </b>
+      </span>
+
+      <br />
+
+      <button>
+        Change theme
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
